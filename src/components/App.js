d6481eb430ccec562f724c65f6303819e684fc88
Marcela Llanos <marcela3llanos@gmail.com>
2019-09-11 17:42:53 -0400

    Preparacion del proyecto tanto de la parte del css  como del index.html donde se agrego bootswatch al proyecto sobre el cual se estara trabajando

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import '../css/App.css';
+import { directive } from '@babel/types';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="App">
+        
+      </div>
+    );}
+}
+
+export default App;
