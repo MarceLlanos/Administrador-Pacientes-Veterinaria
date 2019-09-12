b5f25c6884393c41f2ddf3604bf52f0fa3c8d18a
Marcela Llanos <marcela3llanos@gmail.com>
2019-09-12 01:10:57 -0400

    Se creo el header de la aplicacion

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import Header from './components/Header';
+
+class App extends Component {
+  render(){
+    return (
+      <div className="container">
+        <Header title={"Administrador de Pacientes de Veterinaria"}/>
+      </div>
+    );}
+}
+
+export default App;
